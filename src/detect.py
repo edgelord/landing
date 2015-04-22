7f3c257e7e9d866563606d64d80d3c14642f704c
Akshay Mishra <lck.akshay@gmail.com>
2015-04-21 20:55:21 -0500

    commented

--- src/detect.py
+++ src/detect.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import numpy as np
+import numpy.linalg as la
+
 rsc = "../resources/"
 s1 = rsc + "surface1.dem"
 s2 = rsc + "surface2.dem"
@@ -14,29 +16,34 @@ def load_file(file_name):
         return np.array(np.split(arrayName,500))
 
 
-surf = load_file("../resources/surface1.dem")
+surf = load_file("../resources/surface2.dem")
+
+def py_ang(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'    """
+    cosang = np.dot(v1, v2)
+    sinang = la.norm(np.cross(v1, v2))
+    return np.arctan2(sinang, cosang)
 
 def pix_norm(mtx, pixel):
     x, y = pixel;
     x_scl = x * .2
     y_scl = y * .2
 
-    neighbors = [np.array ([nx*.2, ny*.2,mtx[nx, ny]])
-                 for nx in [x+1, x-1] for ny in [y+1,y-1]]
+    # trying to get neighboring pxls for norms
+    neighbors = [np.array ([nbrx*.2, nbry*.2,mtx[nbrx, nbry]])
+                 for nbrx, nbry in [[x+1, y], [x-1, y]]]
 
+    # getting mid pxl val
     origin = np.array ([x_scl,y_scl,mtx[x,y]])
-    dvec = [nbr - origin for nbr in neighbors]
+    v1, v3, v2, v4 = [nbr - origin for nbr in neighbors]
+    n1 = np.cross(v1,v2)
+    print n1
+    n2 = np.cross(v3,v4)
 
-    return neighbors, origin, dvec
-
-time1 = time();
-print "time 1: " + str()
-v2 = np.array([8.901320,0.1021312,3.0980])
-v1 = np.array([8.901320,0.1081312,7.0980])
-np.cross(v1,v2)
-# [np.cross(v1,v2) for x in range(500*500*4)]
-time2 = time();
-print "time 2: " + str(time1 - time2)
+    z = [0,0,1]
+    a1 = py_ang(n1,z)
+    a2 = py_ang(n2,z)
+    return (a1+a2)/2
 
 
     
