e1fdf701a5c2b185c14207989dd6283559d757dd
Paul Vijayakumar <pvijaya2@illinois.edu>
2015-04-23 21:14:57 -0500

    him

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,10 @@ def pauls_shit(surf):
     Y = np.array([[y*.2 for x in range(500)] for y in range(500)]).flatten()
     Z = np.array([[surf[x,y] for x in range(500)] for y in range(500)]).flatten()
     return np.array ([X, Y, Z])
+
+def load_file(file_name):
+    with open(file_name, "rb") as f:
+        array = np.fromfile(f, np.float32)
+        array.byteswap(True)
+        return np.reshape(array,(500,500))
+
